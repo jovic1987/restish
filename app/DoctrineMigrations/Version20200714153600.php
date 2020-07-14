14e618a911f2ba0911603c6182fd0b293b6b6ecf
Vladan Jovic <jovic1987@gmail.com>
2020-07-14 17:42:19 +0200

    Added new migration

--- app/DoctrineMigrations/Version20200714153600.php
+++ app/DoctrineMigrations/Version20200714153600.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20200709145745 extends AbstractMigration
+class Version20200714153600 extends AbstractMigration
 {
     /**
      * @param Schema $schema
